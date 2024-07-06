764c3f91a90e897c6abc10e41a9ec0fbf25b1f09
Fumiya <75408448+Fumin26u@users.noreply.github.com>
2024-07-07 00:32:50 +0900

    renamed backup to archive, modelに各Tableのカラムをreturnさせるように

--- api/model.py
+++ api/model.py
@@ -22,12 +22,54 @@ class User(db.Model):
 
 class UserImage(db.Model):
     __table__ = db.Table('user_image', db.metadata, autoload_with=db.engine)
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'filename': self.filename,
+            'delete_fg': self.delete_fg,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat()
+        }
 
 class UserImageTag(db.Model):
     __table__ = db.Table('user_image_tag', db.metadata, autoload_with=db.engine)
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_image_id': self.user_image_id,
+            'tag_name': self.tag_name,
+            'tag_name_jp': self.tag_name_jp,
+        }
 
 class UserPlatformAccount(db.Model):
     __table__ = db.Table('user_platform_account', db.metadata, autoload_with=db.engine)
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'platform': self.platform,
+            'platform_id': self.platform_id,
+            'platform_password': self.platform_password,
+            'dl_count': self.dl_count,
+            'get_images_count': self.images_count,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat()
+        }
+        
 
 class UserPlatformAccountDlLog(db.Model):
     __table__ = db.Table('user_platform_account_dl_log', db.metadata, autoload_with=db.engine)
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_platform_account_id': self.user_platform_account_id,
+            'post_id': self.user_image_id,
+            'filename': self.filename,
+            'downloaded_at': self.downloaded_at.isoformat(),
+            'delete_fg': self.delete_fg.isoformat()
+        }
